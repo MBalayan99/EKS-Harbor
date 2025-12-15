5a129c1b931c1a4973cbbe0bb1aa9b0a5be906e2
Mher <48990582+MBalayan99@users.noreply.github.com>
2025-12-15 12:08:41 +0400

    Update providers.tf

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,20 +1,40 @@
-
-provider "aws" { region = "us-east-1" }
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_id
+##################################################
+# AWS Provider
+# Configure AWS region for all resources
+##################################################
+provider "aws" {
+  region = "us-east-1"
 }
 
+##################################################
+# Get EKS cluster info and authentication
+##################################################
+# Retrieve cluster endpoint and CA
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+# Get authentication token for Kubernetes provider
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+##################################################
+# Kubernetes Provider
+# Connect Terraform to the EKS cluster
+##################################################
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  alias                  = "eks"  # alias for modules to reference
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
+##################################################
+# Helm Provider
+# Use the same EKS cluster for Helm deployments
+##################################################
 provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
+  kubernetes = kubernetes.eks  # reference the Kubernetes provider alias
 }
+
